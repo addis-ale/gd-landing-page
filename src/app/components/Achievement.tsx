cce9a32e39b039ac823741fabefc715f4c1feb73
addisalex07 <addis.ugr-4143-15@aau.edu.et>
2025-02-12 20:32:10 +0300

    achievement card completed

--- src/app/components/Achievement.tsx
+++ src/app/components/Achievement.tsx
@@ -1,10 +1,13 @@
 const Achievement = () => {
   return (
-    <div className="relative">
-      <div className="bg-gradient-to-b from-vibrantBlue to-darkBlue z-10"></div>
-      <div>
-        <h1>32K</h1>
-        <p>students Enrolled</p>
+    <div className="relative inline-block">
+      {/* Shadow effect to the bottom and right */}
+      <div className="absolute top-0 left-2 h-[110%] w-full  bg-gradient-to-br from-vibrantBlue to-darkBlue opacity-40  rounded-[24px] "></div>
+
+      {/* Main content */}
+      <div className="bg-white  flex flex-col items-center px-8 py-2 relative z-10 shadow-md rounded-[24px]">
+        <h1 className="font-bold text-[36px] text-darkGray">32K</h1>
+        <p className="text-[16px] text-nowrap mt-[-4px]">Students Enrolled</p>
       </div>
     </div>
   );
